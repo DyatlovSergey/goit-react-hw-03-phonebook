59d8d914d55dd638579309a7c78483f63e58777a
DyatlovSergey <s.dyatlov90@gmail.com>
2022-02-20 19:22:23 +0200

    done and deploy

--- src/App.js
+++ src/App.js
@@ -63,6 +63,20 @@ class App extends React.Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const { contacts, filter } = this.state;
     const visibleContactCards = this.visibleContactCards();
